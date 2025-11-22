b0843dabb4947b967f4efd258ba84a02b036eeb1
Hugo Soares <hugoferreirasoares@gmail.com>
2025-11-22 12:22:07 +0000

    idk: new gradle project as last wasnt working

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@
  *
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/9.2.1/userguide/multi_project_builds.html in the Gradle documentation.
- * This project uses @Incubating APIs which are subject to change.
  */
 
 plugins {
@@ -12,4 +11,4 @@ plugins {
 }
 
 rootProject.name = "SD2526"
-include("lib")
+include("app")
